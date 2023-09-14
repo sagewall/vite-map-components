d48da46c9cc7289714db725bd1223855226e682e
Sage Wall <swall@esri.com>
2023-09-14 16:43:51 -0600

    feat: add legend to layer list and zoom to layer extent

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./style.css";
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import "@arcgis/core/assets/esri/themes/light/main.css";
+import type ListItem from "@arcgis/core/widgets/LayerList/ListItem.js";
+import ListItemPanel from "@arcgis/core/widgets/LayerList/ListItemPanel";
+import type FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
 import { defineCustomElements as defineMapElements } from "@arcgis/map-components/dist/loader";
@@ -9,6 +12,9 @@ defineCalciteElements(window, {
   resourcesUrl: "https://js.arcgis.com/calcite-components/1.7.0/assets",
 });
 defineMapElements();
+
+const arcgisLayerList = document.querySelector("arcgis-layer-list");
+const arcgisMap = document.querySelector("arcgis-map");
 
 const shellPanel = document.querySelector(
   "#shell-panel"
@@ -30,3 +36,37 @@ shellPanel.querySelectorAll("calcite-action").forEach((action) => {
     });
   });
 });
+
+arcgisLayerList?.addEventListener("widgetReady", () => {
+  handleLayerListReady();
+});
+
+arcgisMap?.addEventListener("viewReady", async () => {
+  handleViewReady();
+});
+
+function handleLayerListReady() {
+  const layerList = arcgisLayerList!.widget;
+  layerList.listItemCreatedFunction = listItemCreatedFuntion;
+}
+
+async function handleViewReady() {
+  const mapView = arcgisMap!.view;
+  await mapView.when();
+
+  const hazardAreasFeatureLayer = mapView.map.layers.find(
+    (layer) => layer.title === "Hazard Areas"
+  ) as FeatureLayer;
+
+  const result = await hazardAreasFeatureLayer.queryFeatures();
+  mapView.goTo(result.features);
+}
+
+function listItemCreatedFuntion(event: any) {
+  const item = event.item;
+  if (item.layer.type != "group") {
+    item.panel = {
+      content: "legend",
+    };
+  }
+}
